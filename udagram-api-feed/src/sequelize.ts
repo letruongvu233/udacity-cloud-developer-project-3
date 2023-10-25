479c5544e074b28b4da52288df091964197c014f
Le Truong Vu <letruongvu233@gmail.com>
2023-10-25 23:01:50 +0700

    Update docker file

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -3,11 +3,16 @@ import {config} from './config/config';
 
 
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
-
-  'dialect': config.dialect,
-  'storage': ':memory:',
+  database: config.database,
+  username: config.username,
+  password: config.password,
+  host: config.host,
+  port: 5432,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
 });
